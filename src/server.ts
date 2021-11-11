40d80594a897c2565a23369bbf5bb55a889513bd
Ner01 <36746073+JhonatanT@users.noreply.github.com>
2021-11-11 11:14:26 -0300

    create account instagram robot

--- src/server.ts
+++ src/server.ts
@@ -26,19 +26,32 @@ const fs = require('fs');
       await page1.type('[name="emailOrPhone"]', email)
       await page1.type('[name="fullName"]', "testaos")
       await page1.type('[name="username"]', "wqeqweqw2WW")
-      await page1.type('[name="password"]', "wqeqweqw2WW")
-      setTimeout(async () => { await page.click('[type="submit"]') }, 1000)
+      await page1.type('[name="password"]', "wqeqweqw2WW2")
+      setTimeout(async () => {
+         await page1.click('[type="submit"]')
+         setTimeout(async () => {
 
+          await page1.type('[title="Mês:"]', "janeiro")
+          await page1.type('[title="Dia:"]', "29")
+          await page1.type('[title="Ano:"]', "2000")
+          
+          await page1.click('[class="sqdOP  L3NKy _4pI4F  y3zKF     "]')
+
+          setTimeout(async () => {
+
+            const codigo = await page.evaluate(() => {
+              return document.querySelectorAll('ul span.inboxSubject.small.subject-title.d-none.visable-xs-sm')[1].innerText.slice(0,6)
+            })
+            
+            await page1.type('[type="text"]', codigo)
+            await page1.click('[type="submit"]')
+            
+          },20000)
+
+         },1000)
+        }, 1000)
     }, 1000)
-
-  }, 4000)
-
-
-
-
-
-
-
+  }, 6000)
 
 })();
 
